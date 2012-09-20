3e05a2d40c591480d0c4a79eeb6ba3b57e0b494d
Gaël PORT <gael.port@rbs.fr>
2012-09-20 09:18:20 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -3,15 +3,6 @@ class zone_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
-		try
-		{
-			$scriptReader = import_ScriptReader::getInstance();
-			$scriptReader->executeModuleScript('zone', 'init.xml');
-		}
-		catch (Exception $e)
-		{
-			echo "ERROR: " . $e->getMessage() . "\n";
-			Framework::exception($e);
-		}
+		$this->executeModuleScript('init.xml');
 	}
 }
